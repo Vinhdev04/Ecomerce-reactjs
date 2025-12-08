9b91ed82ea59fcaaac3d37ba2b9c1bdbdbf04578
C.Vinh <pcv.fed@gmail.com>
2025-12-08 21:46:22 +0700

    update: test @mixin in scss

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,11 @@ import './App.css';
 import 'normalize.css';
 import '@styles/app.module.scss';
 import Layout from '@components/Layout/Layout';
-import Header from '@components/Header/Header';
-import Footer from '@components/Footer/Footer';
-import BackToTop from '@components/BackToTop/BackToTop';
 
 function App() {
     return (
         <>
-            <Layout>
-                <Header />
-
-                <div>Hello World</div>
-
-                <BackToTop />
-                <Footer />
-            </Layout>
+            <Layout></Layout>
         </>
     );
 }
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import Header from '../Header/Header.jsx';
+import Footer from '../Footer/Footer.jsx';
+import BackToTop from '../BackToTop/BackToTop.jsx';
+import Button from '../Button/Button.jsx';
 function Layout({ children }) {
     return (
-        <main>
-            <div>{children}</div>
-        </main>
+        <>
+            <Header />
+            <Button />
+            <main>
+                <div>{children}</div>
+            </main>
+
+            <BackToTop />
+
+            <Footer />
+        </>
     );
 }
 
