6d108ea97eddb40fc9ca3da60a89736e863064d0
C.Vinh <pcv.fed@gmail.com>
2025-12-09 15:11:31 +0700

    fix(header): Fix ui header - part-02 [#Header]

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'normalize.css';
 import '@styles/app.module.scss';
+
 import Layout from '@/components/Layout/Layout';
 
 function App() {
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,49 +3,68 @@ import styles from './Header.module.scss';
 import { icons, navItem } from './constant.js';
 import NavIcon from './NavIcon/NavIcon.jsx';
 import MenuItem from './MenuItem/MenuItem.jsx';
-import logo from '@icons/images/logos.png';
+
 import cart from '@icons/svg/cart.svg';
 import reload from '@icons/svg/reload.svg';
 import heart from '@icons/svg/heart.svg';
 function Header() {
-    const { containerBox, containerItem, containerHeader, navLogo } = styles;
+    const {
+        containerBox,
+        containerItem,
+        containerHeader,
+        navLogo,
+        headerContent,
+        titleBox,
+        titleRow,
+        logoFont,
+        descLogo,
+        container
+    } = styles;
     return (
-        <div className={containerHeader}>
-            <div className={containerItem}>
-                <div className={containerBox}>
-                    {icons?.map((item, idx) => {
-                        return (
-                            <NavIcon
-                                key={idx}
-                                href={item.href}
-                                type={item.type}
-                            />
-                        );
-                    })}
+        <div className={container}>
+            <div className={containerHeader}>
+                <div className={containerItem}>
+                    <div className={containerBox}>
+                        {icons?.map((item, idx) => {
+                            return (
+                                <NavIcon
+                                    key={idx}
+                                    href={item.href}
+                                    type={item.type}
+                                />
+                            );
+                        })}
+                    </div>
+
+                    <div className={containerItem}>
+                        {navItem?.slice(0, 3).map((item, idx) => {
+                            return <MenuItem key={idx} title={item.title} />;
+                        })}
+                    </div>
+                </div>
+
+                <div className={headerContent}>
+                    <div className={titleBox}>
+                        <span className={logoFont}>Premium GameXBox</span>
+
+                        <div className={titleRow}>
+                            <span className={descLogo}>Controllers & Gear</span>
+                        </div>
+                    </div>
                 </div>
 
                 <div className={containerItem}>
-                    {navItem?.slice(0, 3).map((item, idx) => {
-                        return <MenuItem key={idx} title={item.title} />;
-                    })}
-                </div>
-            </div>
-
-            <div className=''>
-                <img src={logo} alt='logo' className={navLogo} />
-            </div>
-
-            <div className={containerItem}>
-                {' '}
-                <div className={containerItem}>
-                    {navItem?.slice(3, navItem.length).map((item, idx) => {
-                        return <MenuItem key={idx} title={item.title} />;
-                    })}
-                </div>
-                <div className={containerBox}>
-                    <img src={reload} width={26} height={26} />
-                    <img src={heart} width={26} height={26} />
-                    <img src={cart} width={26} height={26} />
+                    {' '}
+                    <div className={containerItem}>
+                        {navItem?.slice(3, navItem.length).map((item, idx) => {
+                            return <MenuItem key={idx} title={item.title} />;
+                        })}
+                    </div>
+                    <div className={containerBox}>
+                        <img src={reload} width={26} height={26} />
+                        <img src={heart} width={26} height={26} />
+                        <img src={cart} width={26} height={26} />
+                    </div>
                 </div>
             </div>
         </div>
