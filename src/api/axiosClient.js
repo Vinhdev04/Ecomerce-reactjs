46f62c51c58b3fcf828364a0b67fa45d6544a534
C.Vinh <pcv.fed@gmail.com>
2026-02-01 17:43:38 +0700

    update axiosclinet

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:3000/api/',
+    baseURL: import.meta.env.VITE_API_URL ||  import.meta.env.VITE_API_URL_DEPLOY ,
     timeout: 10000,
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
