05c0c13735dffbcfef3ebc7041d2c7cbfd6157e9
C.Vinh <pcv.fed@gmail.com>
2026-03-04 20:35:17 +0700

    fix(header): cải thiện responsive navbar - mobile menu open/close, khóa scroll, ESC để đóng; chuyển icon mạng xã hội thành link; đảm bảo trạng thái open hoạt động với CSS Modules

--- src/components/Header/NavIcon/NavIcon.jsx
+++ src/components/Header/NavIcon/NavIcon.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
-
 import styles from '../Header.module.scss';
-import { FaTiktok } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
+import { FaTiktok } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { FaFacebook } from 'react-icons/fa';
 
 function NavIcon({ type, href }) {
-    const { navIcons } = styles;
-    const handleShowIcon = (type) => {
-        switch (type) {
-            case 'fb':
-                return <FaFacebook />;
-            case 'ins':
-                return <FaInstagram />;
-            case 'tiktok':
-                return  <FaTiktok />;
-            default:
-                throw new Error('Error when render icon');
-        }
-    };
-    return (
-        <div className={navIcons}>
-           {handleShowIcon(type)}
-        </div>
-    );
+  const { navIcons } = styles;
+  const handleShowIcon = (type) => {
+    switch (type) {
+      case 'fb':
+        return <FaFacebook />;
+      case 'ins':
+        return <FaInstagram />;
+      case 'tiktok':
+        return <FaTiktok />;
+      default:
+        throw new Error('Error when render icon');
+    }
+  };
+  return (
+    <a className={navIcons} href={href} target="_blank" rel="noreferrer noopener" aria-label={type}>
+      {handleShowIcon(type)}
+    </a>
+  );
 }
 
 export default NavIcon;
